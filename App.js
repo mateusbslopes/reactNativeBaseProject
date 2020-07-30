d34acde2c89191a86c2decb8b1b67fbd16fc9826
Mateus Lopes <mateusbslopes@gmail.com>
2020-07-30 17:50:51 -0300

    chore: Project with expo ejected

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Text,
+} from 'react-native';
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <Text>Teste</Text>
+    </>
+  );
+};
+
+
+export default App;
